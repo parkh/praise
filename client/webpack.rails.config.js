a32d07337c41a822afa21cffecac199802c5942b
Dan Parkhomenko <parkh@users.noreply.github.com>
2016-03-24 15:50:10 +0200

    webpack setted up

---
+++ client/webpack.rails.config.js
@@ -0,0 +1,8 @@
+const config = require('./webpack.common.config');
+
+config.output = {
+  filename: 'client-bundle.js',
+  path: '../app/assets/javascripts/generated'
+};
+
+module.exports = config;
